2403d9c76a15392b550fba110689f0098e681df5
ttan0408 <71804399+ttan0408@users.noreply.github.com>
2020-11-14 14:31:19 -0600

    Export Retirement Eligibility CSV

--- Queries/Data/schema.sql
+++ Queries/Data/schema.sql
@@ -1,6 +1,6 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
+     dept_no VARCHAR(40) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
@@ -8,6 +8,8 @@ CREATE TABLE departments (
 
 --SELECT *
 --FROM departments
+
+--DROP TABLE employees CASCADE;
 
 CREATE TABLE employees (
 	 emp_no INT NOT NULL,
@@ -23,7 +25,7 @@ CREATE TABLE employees (
 --FROM employees
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(40) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -43,3 +45,59 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+--SELECT *
+--FROM salaries
+
+CREATE TABLE title (
+    emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+    PRIMARY KEY (emp_no, from_date)
+);
+
+--DROP TABLE title CASCADE;
+
+--SELECT *
+--FROM title 
+
+CREATE TABLE dept_emp (
+    dept_no VARCHAR(40) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+--SELECT *
+--FROM dept_emp 
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1952-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new tabble for Retirement eligibility and save it to retirement_info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
