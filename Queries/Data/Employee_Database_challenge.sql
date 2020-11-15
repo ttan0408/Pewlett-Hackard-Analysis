71220fe12d19628717b8ca6e9bc24c031d65040b
ttan0408 <71804399+ttan0408@users.noreply.github.com>
2020-11-14 21:54:17 -0600

    Add Mentorship Eligibility

--- Queries/Data/Employee_Database_challenge.sql
+++ Queries/Data/Employee_Database_challenge.sql
@@ -15,3 +15,22 @@ GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
 
 SELECT * FROM retiring_titles
+
+--Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN title AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty
